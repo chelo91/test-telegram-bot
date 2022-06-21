2c810363753b172def40447c22ea97dd9688a1bf
Chelo <marzelo.aguirre@gmail.com>
2022-06-21 01:20:48 -0300

    funciones basicas

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
 const { Telegraf } = require('telegraf')
-const session = require('telegraf/session')
+const { MongoClient } = require('mongodb');
+const { session } = require('telegraf-session-mongodb');
+
 const controller_score = require('./controllers/controllerScore')
 
 require("dotenv").config()
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.use(session())
-bot.on('text', (ctx) => {
-  ctx.session.counter = ctx.session.counter || 0
-  ctx.session.counter++
-  return ctx.reply(`Message counter:${ctx.session.counter}`)
-})
+MongoClient.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(client => {
+
+    const db = client.db();
+    bot.use(session(db, { collectionName: 'sessions' }));
+
+    bot.start((ctx) => ctx.reply('https://giphy.com/gifs/cat-hello-hey-VOPK1BqsMEJRS'))
+
+    bot.help((ctx) => ctx.reply('win - Add 3 points \n' +
+      'draw - Add 1 point \n' +
+      'points - Show the score for a person \n' +
+      'table - Show all scores \n' +
+      'restart - Restart scores'));
+
+    bot.command('/win', (ctx) => controller_score.win(ctx));
+
+    bot.command('/draw', (ctx) => controller_score.draw(ctx));
+
+    bot.command('/points', (ctx) => controller_score.points(ctx));
+
+    bot.command('/restart', (ctx) => controller_score.restart(ctx));
+
+    bot.command('/table', (ctx) => controller_score.table(ctx));
+
+    bot.command('/set', (ctx) => controller_score.set(ctx));
+
+  });
 
 bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
---
+++ controllers/controllerScore.js
@@ -0,0 +1,91 @@
+exports.win = async function (ctx) {
+    await givePoints(ctx, 3);
+};
+
+exports.draw = async function (ctx) {
+    await givePoints(ctx, 1);
+};
+
+exports.set = async function (ctx) {
+    if (ctx.message.from.id == 589215) {
+        var array = ctx.message.text.split(" ");
+        await ctx.reply(showPoints({ name: array[1], points: array[2] }));
+    } else {
+        await ctx.reply('https://giphy.com/gifs/ian-mckellen-nlcJ4MkoG3Tri');
+    }
+};
+
+exports.points = async function (ctx) {
+    const array = ctx.session.table || [];
+    var name = ctx.message.from.first_name;
+
+    i = findPlayerByName(array, name);
+
+    if (i == -1) {
+        i = createPlayer(array, name, 0);
+    }
+
+    await ctx.reply(showPoints(array[i]));
+    ctx.session.table = array;
+};
+
+exports.restart = async function (ctx) {
+    ctx.session.table = [];
+    await ctx.reply('Tabla reiniciada');
+};
+
+exports.table = async function (ctx) {
+    const array = ctx.session.table || [];
+    var AsciiTable = require('ascii-table');
+    var table = new AsciiTable('The Cat League');
+
+    table.setHeading('', 'Jugador', 'Puntos')
+
+    for (let i = 0; i < array.length; i++) {
+        table.addRow(i + 1, array[i].name, array[i].points);
+    }
+
+    await ctx.replyWithMarkdown("```" + table.toString() + "```");
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+async function givePoints(ctx, points) {
+    const array = ctx.session.table || [];
+    var name = ctx.message.from.first_name;
+
+    i = sumPointsInArray(array, name, points);
+
+    await ctx.reply(showPoints(array[i]));
+    ctx.session.table = array;
+}
+
+function sumPointsInArray(array, name, points) {
+    i = findPlayerByName(array, name);
+    if (i != -1) {
+        array[i].points += points;
+        return i;
+    } else {
+        createPlayer(array, name, points);
+        return array.length - 1;
+    }
+}
+
+function showPoints(player) {
+    return `El jugador ${player.name} va ${player.points} puntos`;
+}
+
+function findPlayerByName(array, name) {
+    found = -1;
+    for (var i = 0; i < array.length && found == -1; i++) {
+        if (array[i].name === name) {
+            found = i;
+        }
+    }
+    return found;
+}
+
+function createPlayer(array, name, points) {
+    array.push({ name: name, points: points });
+    return array.length - 1;
+}
